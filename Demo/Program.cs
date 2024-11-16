47779faa0d35b09742fa3a4b8dec2436134d882f
nd <nd1012@users.noreply.github.com>
2024-11-16 09:25:12 +0100

    Update
    + Added `ReflectionExtensions.(Is|MayBe)Disposable`

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,8 +3,8 @@ using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 // Definieren Sie hier Ihre persönlichen API Zugangsdaten
-string key = "Ihr API Schlüssel",// Nutzen Sie bitte Ihren persönlichen REST API Schlüssel, der mit den entsprechenden Scopes erstellt wurde
-    uri = "https://post.gotomaxx.com/PORTALNAME/service/rest/outgoing";// Nutzen Sie bitte die korrekte URI zu Ihrem Portal
+string key = "Ihr API Schlüssel",// Nutzen Sie bitte Ihren persönlichen REST API Schlüssel, der für die entsprechenden erforderlichen Scopes (hier pdf.management) erstellt wurde
+    uri = "https://post.gotomaxx.com/PORTALNAME/service/rest/outgoing";// Nutzen Sie bitte die korrekte URI zu Ihrem Portal ohne abschließenden Slash
 
 // Senden eines REST requests - hier: Liste der gesendeten Dokumente abrufen (mit allen Standardeinstellungen)
 using HttpClient client = new();
@@ -17,7 +17,7 @@ response.EnsureSuccessStatusCode();
 
 // Verarbeiten des Response
 GotomaxxPdfUploadRestResponse[] files = await response.Content.ReadFromJsonAsync<GotomaxxPdfUploadRestResponse[]>()
-    ?? throw new InvalidDataException("Ungültiger response");
+    ?? throw new InvalidDataException("Ungültiger Response");
 
 Console.WriteLine($"Anzahl PDF Dokumente: {files.Length}");
 foreach (GotomaxxPdfUploadRestResponse file in files)
